424958608f328b8a454c63d1312ac21727697453
faiq <sutiono.plague@gmail.com>
2025-05-25 21:04:11 +0700

    add menu

--- helloworld.go
+++ helloworld.go
@@ -1,8 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	menu := true
+	gudang := Gudang{}
+	for menu {
+		var pilihan int
+		fmt.Println("Menu gudang:")
+		fmt.Println(`Silahkan pilih menu: 
+		1. Tambah Barang
+		2. Tampilkan Barang
+		3. Totalkan Barang
+		4. Keluar`)
+
+		switch pilihan {
+		case 1:
+			caseOne := true
+			var barang string
+			var jumlah int
+			for caseOne {
+				if barang == "n" || strconv.Itoa(jumlah) == "n" {
+					caseOne = false
+				} else {
+					fmt.Println("Masukkan nama barang dan jumlah:")
+					fmt.Println("Nama Barang: ")
+					fmt.Scan(&barang)
+					fmt.Println("Jumlah Barang: ")
+					fmt.Scan(&jumlah)
+					inputBarang := Barang{barang, jumlah}
+					Tambah_barang(&gudang, inputBarang)
+					fmt.Println(`ketik 'n' untuk mengakhiri...`)
+				}
+			}
+		case 2:
+			Tampilkan_barang(gudang)
+
+		case 3:
+			Total_barang(gudang)
+
+		default:
+			fmt.Println("app ditutup..")
+			menu = false
+		}
+
+	}
 	RunGudang()
 }
